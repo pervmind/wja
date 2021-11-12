2f5b90ba1c4891afa847de339f66fa7a6a28bea5
pervmind <3msabdullah@gmail.com>
2021-11-12 23:09:25 +0200

    update 4
    added the updateUI function to update the ui with the new data coming from the server

--- website/app.js
+++ website/app.js
@@ -1,6 +1,9 @@
 /* Global Variables */
 const key = 'ee9b94c3b4f702c8fd14c4ebe5782bf0';
 const button = document.getElementById('generate');
+const htmlDate = document.getElementById('date');
+const htmlTemp = document.getElementById('temp');
+const htmlUserResponse = document.getElementById('content');
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+1+'.'+ d.getDate()+'.'+ d.getFullYear();
@@ -14,8 +17,14 @@ button.addEventListener('click', async()=>{
     const info = await response.json()
     const temp = info.main.temp;
     const output = {temp, newDate, userResponse};
-    postedData('/postData', output);
+    postedData('/postData', output)
+    const final =  await dataUpdate('/getProjectData');
+    updateUI(final);
 });
+
+
+
+
 const postedData = async function postWeatherData(url = '', data ={}){
     const post = await fetch(url,{
         method : 'POST',
@@ -26,11 +35,28 @@ const postedData = async function postWeatherData(url = '', data ={}){
         body:JSON.stringify(data),
     });
     try{
-        const newData =post.json();
-        return newData
+        const sentData = await post.json();
+        return sentData
     }catch(error){
         console.log('error',error);
     }
 
     
+}
+
+
+const dataUpdate = async function getWeatherData(url = ''){
+    const update = await fetch(url);
+    try{
+        const newData = await update.json();
+        return newData
+    }catch(error){
+        console.log('error',error);
+    }
+}
+
+async function updateUI(object){
+    htmlDate.textContent = await object.newDate;
+    htmlTemp.textContent = await object.temp;
+    htmlUserResponse.textContent = await object.userResponse;
 }
